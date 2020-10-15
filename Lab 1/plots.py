615cd5dbbd345aa9fc8819e5655269c373fa5b36
VolodiaYavorskyi <43553028+VolodiaYavorskyi@users.noreply.github.com>
2020-10-15 18:53:54 +0300

    first lab remake

--- Lab 1/plots.py
+++ Lab 1/plots.py
@@ -5,12 +5,28 @@ def numericPlot(dataframe, column, pl_type):
     fig, ax = plt.subplots()
 
     if pl_type == "bar":
+        dataframe = dataframe.groupby(dataframe.index).max()
         ax.bar(dataframe.index, dataframe[column], label=column)
         ax.set_title("Bar plot")
     elif pl_type == "scatter":
+        dataframe = dataframe.groupby(dataframe.index).max()
         ax.scatter(dataframe.index, dataframe[column], label=column)
         ax.set_title("Scatter plot")
+    elif pl_type == "hist":
+        ax.hist(dataframe[column], bins=10, label=column)
+        ax.set_title("Histogram")
+        ax.set_xlabel(column)
+        plt.show()
+        return
+    elif pl_type == "pie":
+        stats = dataframe.value_counts(column)
+        plt.pie(stats, autopct='%1.1f%%')
+        ax.legend(stats.index, fontsize="x-small", loc='best', bbox_to_anchor=(0.8, 0.65, 0.5, 0.5))
+        ax.set_title("Pie chart")
+        plt.show()
+        return
     else:
+        dataframe = dataframe.groupby(dataframe.index).max()
         ax.plot(dataframe.index, dataframe[column], label=column)
         ax.set_title("Line plot")
     ax.legend()
@@ -21,6 +37,7 @@ def numericPlot(dataframe, column, pl_type):
 
 
 def numericPlot2(dataframe, column1, column2, pl_type):
+    dataframe = dataframe.groupby(dataframe.index).max()
     fig, ax = plt.subplots()
 
     if pl_type == "bar":
@@ -39,13 +56,4 @@ def numericPlot2(dataframe, column1, column2, pl_type):
     ax.set_xlabel("Date")
     plt.xticks(dataframe.index, rotation='vertical')
     plt.subplots_adjust(bottom=0.3)
-    plt.show()
-
-
-def pieChart(dataframe, column):
-    stats = dataframe.value_counts(column)
-    fig, ax = plt.subplots()
-    plt.pie(stats, labels=stats.index, autopct='%1.1f%%')
-    ax.legend(fontsize="x-small", loc='best', bbox_to_anchor=(0.8, 0.65, 0.5, 0.5))
-    ax.set_title("Pie chart")
     plt.show()
